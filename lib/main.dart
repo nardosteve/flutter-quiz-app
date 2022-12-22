537e25fa0afd0c168702f782b7a3313ae4565c37
nardosteve <jackcale50@gmail.com>
2022-12-22 16:48:16 +0300

    Setting up the onPressed event listener

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,48 @@ void main() {
 //Stateful - changes overtime
 //Every widget has to extend stateless/stateful
 class MyApp extends StatelessWidget {
+  //Creating a function
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   //BuildContext - special type of object
   //Build method is always responsible for returning a new widget
   @override
   Widget build(BuildContext context) {
+    //Creating a list/array
+    var questions = [
+      'Who is the president of USA?',
+      'What are the colors of the Kenyan flag',
+    ];
+
     //MaterialApp - based setup to combine th eapp into a real app
     //Constructor name parameters
-
     //Scaffold (widget) - base page design
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.purple,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          backgroundColor: Colors.red,
+        ),
+        body: Column(
+          children: [
+            const Text('The question'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer1'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer2'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer3'),
+            ),
+          ],
+        ),
       ),
-      body: Text('Default App'),
-    ));
+    );
   }
 }
